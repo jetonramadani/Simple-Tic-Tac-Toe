d8cbd6d8605b7d0b33b1df4e2138dcdabe3e5871
jetonramadani <jeton.ramadani@students.finki.ukim.mk>
2021-06-20 13:51:13 +0200

    Work on project. Stage 2/5: The user is the gamemaster'

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,10 +1,24 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void printBoard(char[] plays) {
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(plays[i * 3 + j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         // write your code here
-        System.out.println("X O X\n" +
-                "O X O\n" +
-                "X X O");
+        System.out.print("Enter cells: ");
+        char[] plays = sc.next().toCharArray();
+        printBoard(plays);
     }
 }
