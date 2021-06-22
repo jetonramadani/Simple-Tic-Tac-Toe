7f03e72b64f2bb1f8c84afa394a8c264891ef608
jetonramadani <jeton.ramadani@students.finki.ukim.mk>
2021-06-22 14:28:54 +0200

    Work on project. Stage 4/5: First move!

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,27 +1,86 @@
 package tictactoe;
 
+import java.util.HashMap;
 import java.util.Scanner;
-
-public class Main {
-    public static int checkOk(char x, char y, char z) {
+class TicTacToe {
+    private char[] plays;
+    private final Scanner sc;
+    private int x_;
+    private int o_;
+    private boolean xMove;
+    static final int [][] winCombos = {
+            {0, 1, 2},
+            {0, 3, 6},
+            {0, 4, 8},
+            {1, 4, 7},
+            {3, 4, 5},
+            {2, 5, 8},
+            {2, 4, 6},
+            {6, 7, 8}
+    };
+    static final HashMap<String, Integer> movesToIndex =
+            new HashMap<>() {
+                {
+                    put("1.1", 0);
+                    put("1.2", 1);
+                    put("1.3", 2);
+                    put("2.1", 3);
+                    put("2.2", 4);
+                    put("2.3", 5);
+                    put("3.1", 6);
+                    put("3.2", 7);
+                    put("3.3", 8);
+                }
+            };
+    TicTacToe(Scanner sc) {
+        this.sc = sc;
+        setCells();
+    }
+    private void setCells() {
+        System.out.print("Enter cells: ");
+        plays = sc.next().toCharArray();
+        for (char ch : plays) {
+            if (ch == 'X') {
+                ++x_;
+            } else if (ch == 'O') {
+                ++o_;
+            }
+        }
+//        xMove = x_ <= o_;
+        xMove = true;
+    }
+    public void move() {
+        while (true) {
+            System.out.print("Enter the coordinates: ");
+            String search = sc.nextInt() + "." + sc.nextInt();
+            try {
+                Double.parseDouble(search);
+                if (movesToIndex.containsKey(search)) {
+                    int ind = movesToIndex.get(search);
+                    if (plays[ind] == '_') {
+                        plays[ind] = xMove ? 'X' : 'O';
+                        break;
+                    } else {
+                        System.out.println("This cell is occupied! Choose another one!");
+                    }
+                } else {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                }
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
+            }
+        }
+        printBoard();
+    }
+    private int checkOk(char x, char y, char z) {
         if (x != '_' && x == y && x == z) {
             return x == 'X' ? 1 : -1;
         }
         return 0;
     }
-    public static char checkWin(char[] plays) {
+    private char checkWin() {
         int x = 0;
         int o = 0;
-        int[][] winCombos = {
-                {0, 1, 2},
-                {0, 3, 6},
-                {0, 4, 8},
-                {1, 4, 7},
-                {3, 4, 5},
-                {2, 5, 8},
-                {2, 4, 6},
-                {6, 7, 8}
-        };
         for (int[] combo : winCombos) {
             int res = checkOk(plays[combo[0]],
                     plays[combo[1]],
@@ -39,24 +98,16 @@ public class Main {
         } else if (x != 0) {
             return 'X';
         } else {
-            int x_ = 0;
-            int o_ = 0;
-            for (char ch : plays) {
-                if (ch == 'X') {
-                    ++x_;
-                } else if (ch == 'O') {
-                    ++o_;
-                }
-            }
             String gameState = new String(plays);
             if (gameState.contains("_")) {
                 return x_ == o_ || x_ == o_ + 1 ? 'N' : 'I';
             }
             return x_ == o_ || x_ == o_ + 1 ? 'D' : 'I';
         }
+
     }
-    public static String getGameCheck(char[] plays) {
-        switch (checkWin(plays)) {
+    public String getGameCheck() {
+        switch (checkWin()) {
             case 'X' :
                 return "X wins";
             case 'O' :
@@ -71,7 +122,7 @@ public class Main {
                 return "";
         }
     }
-    public static void printBoard(char[] plays) {
+    public void printBoard() {
         System.out.println("---------");
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
@@ -82,12 +133,15 @@ public class Main {
         }
         System.out.println("---------");
     }
+}
+public class Main {
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // write your code here
-        System.out.print("Enter cells: ");
-        char[] plays = sc.next().toCharArray();
-        printBoard(plays);
-        System.out.println(getGameCheck(plays));
+        TicTacToe ticTacToe = new TicTacToe(sc);
+        ticTacToe.printBoard();
+        ticTacToe.move();
     }
 }
